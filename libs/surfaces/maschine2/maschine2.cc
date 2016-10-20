fd44d621e2c8a3da36af200a079135efcd058b53
Robin Gareus <robin@gareus.org>
2016-10-20 15:55:00 +0200

    skeleton for NI Maschine MK2

---
+++ libs/surfaces/maschine2/maschine2.cc
@@ -0,0 +1,186 @@
+/*
+ * Copyright (C) 2016 Robin Gareus <robin@gareus.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#include "pbd/compose.h"
+#include "pbd/error.h"
+#include "pbd/i18n.h"
+
+#include "ardour/session.h"
+
+#include "pbd/abstract_ui.cc" // instantiate template
+
+#include "maschine2.h"
+
+using namespace ARDOUR;
+using namespace PBD;
+using namespace ArdourSurface;
+
+Maschine2::Maschine2 (ARDOUR::Session& s)
+	: ControlProtocol (s, string (X_("Native Instruments Maschine Controller MK2")))
+	, AbstractUI<Maschine2Request> (name())
+	, handle (0)
+{
+	if (hid_init()) {
+		throw Maschine2Exception ("HIDAPI initialization failed");
+	}
+	run_event_loop ();
+}
+
+Maschine2::~Maschine2 ()
+{
+	stop ();
+	hid_exit ();
+}
+
+void*
+Maschine2::request_factory (uint32_t num_requests)
+{
+	return request_buffer_factory (num_requests);
+}
+
+void
+Maschine2::do_request (Maschine2Request* req)
+{
+	if (req->type == CallSlot) {
+		call_slot (MISSING_INVALIDATOR, req->the_slot);
+	} else if (req->type == Quit) {
+		stop ();
+	}
+}
+
+int
+Maschine2::set_active (bool yn)
+{
+	if (yn == active()) {
+		return 0;
+	}
+
+	if (yn) {
+		if (start ()) {
+			return -1;
+		}
+	} else {
+		if (stop ()) {
+			return -1;
+		}
+	}
+
+	ControlProtocol::set_active (yn);
+	return 0;
+}
+
+XMLNode&
+Maschine2::get_state()
+{
+	XMLNode& node (ControlProtocol::get_state());
+	return node;
+}
+
+int
+Maschine2::set_state (const XMLNode & node, int version)
+{
+	if (ControlProtocol::set_state (node, version)) {
+		return -1;
+	}
+	return 0;
+}
+
+int
+Maschine2::start ()
+{
+	handle = hid_open (0x17cc, 0x1140, NULL);
+	if (!handle) {
+		error << _("Cannot find or connect to Maschine2");
+		return -1;
+	}
+
+	Glib::RefPtr<Glib::TimeoutSource> write_timeout = Glib::TimeoutSource::create (40);
+	write_connection = write_timeout->connect (sigc::mem_fun (*this, &Maschine2::dev_write));
+	write_timeout->attach (main_loop()->get_context());
+
+	Glib::RefPtr<Glib::TimeoutSource> read_timeout = Glib::TimeoutSource::create (1);
+	read_connection = read_timeout->connect (sigc::mem_fun (*this, &Maschine2::dev_read));
+	read_timeout->attach (main_loop()->get_context());
+
+	return 0;
+}
+
+int
+Maschine2::stop ()
+{
+	hid_close (handle);
+	handle = 0;
+
+	read_connection.disconnect ();
+	write_connection.disconnect ();
+
+	stop_event_loop ();
+	return 0;
+}
+
+void
+Maschine2::thread_init ()
+{
+	pthread_set_name (event_loop_name().c_str());
+	ARDOUR::SessionEvent::create_per_thread_pool (event_loop_name(), 128);
+	PBD::notify_event_loops_about_thread_creation (pthread_self(), event_loop_name(), 128);
+
+	struct sched_param rtparam;
+	memset (&rtparam, 0, sizeof (rtparam));
+	rtparam.sched_priority = 9; /* XXX should be relative to audio (JACK) thread */
+	if (pthread_setschedparam (pthread_self(), SCHED_FIFO, &rtparam) != 0) {
+		// do we care? not particularly.
+	}
+}
+
+void
+Maschine2::run_event_loop ()
+{
+	BaseUI::run ();
+}
+
+void
+Maschine2::stop_event_loop ()
+{
+	BaseUI::quit ();
+}
+
+bool
+Maschine2::dev_read ()
+{
+	uint8_t buf[256];
+	int res = hid_read(handle, buf, 256);
+	if (res < 1) {
+		return true;
+	}
+	printf ("%d\n", res);
+	return true;
+}
+
+bool
+Maschine2::dev_write ()
+{
+	return true;
+}
---
+++ libs/surfaces/maschine2/maschine2.h
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2016 Robin Gareus <robin@gareus.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef _ardour_surfaces_maschine2_h_
+#define _ardour_surfaces_maschine2_h_
+
+#ifdef PLATFORM_WINDOWS
+#include <windows.h>
+#endif
+
+#include <hidapi.h>
+
+#define ABSTRACT_UI_EXPORTS
+#include "pbd/abstract_ui.h"
+#include "ardour/types.h"
+#include "control_protocol/control_protocol.h"
+
+namespace ArdourSurface {
+
+class Maschine2Exception : public std::exception
+{
+	public:
+		Maschine2Exception (const std::string& msg) : _msg( msg ) { }
+		virtual ~Maschine2Exception() throw () {}
+		const char* what() const throw () { return _msg.c_str(); }
+	private:
+		std::string _msg;
+};
+
+struct Maschine2Request : public BaseUI::BaseRequestObject {
+  public:
+	Maschine2Request () {}
+	~Maschine2Request() {}
+};
+
+class Maschine2: public ARDOUR::ControlProtocol, public AbstractUI<Maschine2Request>
+{
+	public:
+		Maschine2 (ARDOUR::Session&);
+		~Maschine2 ();
+
+		static void* request_factory (uint32_t);
+
+#if 0
+		bool has_editor () const { return false; }
+		void* get_gui () const;
+		void  tear_down_gui ();
+#endif
+
+		int set_active (bool yn);
+		XMLNode& get_state();
+		int set_state (const XMLNode & node, int version);
+
+	private:
+		void do_request (Maschine2Request*);
+
+		int start ();
+		int stop ();
+
+		void thread_init ();
+		void run_event_loop ();
+		void stop_event_loop ();
+
+		sigc::connection read_connection;
+		sigc::connection write_connection;
+
+		bool dev_write ();
+		bool dev_read ();
+
+		hid_device* handle;
+};
+
+} /* namespace */
+#endif /* _ardour_surfaces_maschine2_h_*/
